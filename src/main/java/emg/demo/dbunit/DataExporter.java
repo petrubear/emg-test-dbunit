502b66a93dbaf4d27b954e6d7a0e75429d21ace9
petrubear <edison.martinez@gmail.com>
2016-04-28 16:29:43 -0500

    metodos para exportar diferentes fuentes de datos

--- src/main/java/emg/demo/dbunit/DataExporter.java
+++ src/main/java/emg/demo/dbunit/DataExporter.java
@@ -5,40 +5,67 @@ import java.sql.Connection;
 
 import javax.sql.DataSource;
 
-import org.apache.commons.dbcp2.BasicDataSource;
+import org.dbunit.database.DatabaseConfig;
 import org.dbunit.database.DatabaseConnection;
 import org.dbunit.database.IDatabaseConnection;
 import org.dbunit.database.QueryDataSet;
 import org.dbunit.database.search.TablesDependencyHelper;
 import org.dbunit.dataset.IDataSet;
 import org.dbunit.dataset.xml.FlatXmlDataSet;
+import org.dbunit.ext.oracle.OracleDataTypeFactory;
 
 public class DataExporter {
-	public void exportData(DataSource datasource) throws Exception {
-		// database connection
-		// Class driverClass = Class.forName("org.hsqldb.jdbcDriver");
-		// Connection jdbcConnection =
-		// DriverManager.getConnection("jdbc:hsqldb:sample", "sa", "");
-		Connection jdbcConnection = datasource.getConnection();
-		IDatabaseConnection connection = new DatabaseConnection(jdbcConnection);
+	private DataSource datasource;
 
-		try {
-			// partial database export
-//			QueryDataSet partialDataSet = new QueryDataSet(connection);
-//			partialDataSet.addTable("FOO", "select * from tcrb_order");
-//			partialDataSet.addTable("BAR");
-//			FlatXmlDataSet.write(partialDataSet, new FileOutputStream("partial.xml"));
+	public DataSource getDatasource() {
+		return datasource;
+	}
 
-			// full database export
-			//IDataSet fullDataSet = connection.createDataSet();
-			//FlatXmlDataSet.write(fullDataSet, new FileOutputStream("full.xml"));
+	public void setDatasource(DataSource datasource) {
+		this.datasource = datasource;
+	}
 
-			// dependent tables database export: export table X and all tables
-			// that
-			// have a PK which is a FK on X, in the right order for insertion
-			String[] depTableNames = TablesDependencyHelper.getAllDependentTables(connection, "TCRB_ORDER");
+	public DataExporter(DataSource datasource) {
+		this.datasource = datasource;
+	}
+
+	public void exportTableAndDependencies(String tableName, String fileName) throws Exception {
+		try (Connection jdbcConnection = datasource.getConnection()) {
+			IDatabaseConnection connection = new DatabaseConnection(jdbcConnection);
+			DatabaseConfig memConfig = connection.getConfig();
+			memConfig.setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new OracleDataTypeFactory());
+
+			String[] depTableNames = TablesDependencyHelper.getAllDependentTables(connection, tableName);
 			IDataSet depDataset = connection.createDataSet(depTableNames);
-			FlatXmlDataSet.write(depDataset, new FileOutputStream("dependents.xml"));
+			FlatXmlDataSet.write(depDataset, new FileOutputStream(fileName));
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public void exportFullDatabase(String fileName) throws Exception {
+		try (Connection jdbcConnection = datasource.getConnection()) {
+			IDatabaseConnection connection = new DatabaseConnection(jdbcConnection);
+			DatabaseConfig memConfig = connection.getConfig();
+			memConfig.setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new OracleDataTypeFactory());
+
+			IDataSet fullDataSet = connection.createDataSet();
+			FlatXmlDataSet.write(fullDataSet, new FileOutputStream(fileName));
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public void exportTableQuery(String table, String sql, String fileName) throws Exception {
+		try (Connection jdbcConnection = datasource.getConnection()) {
+			IDatabaseConnection connection = new DatabaseConnection(jdbcConnection);
+			DatabaseConfig memConfig = connection.getConfig();
+			memConfig.setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new OracleDataTypeFactory());
+			
+			QueryDataSet partialDataSet = new QueryDataSet(connection);
+			partialDataSet.addTable(table, sql);
+			// partialDataSet.addTable("BAR");
+			FlatXmlDataSet.write(partialDataSet, new FileOutputStream(fileName));
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
--- src/test/java/emg/demo/dbunit/DataExporterTest.java
+++ src/test/java/emg/demo/dbunit/DataExporterTest.java
@@ -6,14 +6,31 @@ import org.apache.commons.dbcp2.BasicDataSource;
 import org.junit.Test;
 
 public class DataExporterTest {
-
 	@Test
-	public void exportDataTest() throws Exception {
-		DataExporter dex = new DataExporter();
-		dex.exportData(getDataSource());
+	public void testExportTableQuery() throws Exception {
+		DataExporter dex = new DataExporter(eefisaDataSource());
+		final String table = "UniversalCatalog";
+		final String sql = "SELECT UCT.FIELD_NAME AS FIELD_NAME, UCT.TARGET_TABLE AS T_TABLE, "
+				+ "UCT.TARGET_FIELD AS T_FIELD, SSY.DATA_SOURCE_ID AS DATA_SOURCE, "
+				+ "UCT.APPLICATION_ID AS APPLICATION_ID, " + "ELE.NAME AS ELEMENT_NAME, " + "FMT.NAME AS FORMAT_NAME, "
+				+ "REG.NAME REGISTER_NAME " + "FROM " + "TPAR_UNIVERSAL_CATALOG UCT, " + "TAPD_APPLICATION APP, "
+				+ "TAPD_SOURCE_SYSTEM SSY, " + "TPAR_ELEMENT ELE, " + "TPAR_REGISTER REG, " + "TPAR_FILE_FORMAT FMT "
+				+ "WHERE " + "UCT.APPLICATION_ID = APP.APPLICATION_ID "
+				+ "AND APP.SOURCE_SYSTEM_ID = SSY.SOURCE_SYSTEM_ID " + "AND UCT.FIELD_NAME = ELE.FIELD_NAME "
+				+ "AND REG.ID_REGISTER = ELE.ID_REGISTER " + "AND FMT.ID_FILE_FORMAT = REG.ID_FILE_FORMAT";
+		final String fileName = "/Users/edison/Tmp/export/ucatalog.xml";
+		dex.exportTableQuery(table, sql, fileName);
 	}
 
-	private DataSource getDataSource() {
+	@Test
+	public void testExportTableAndDependencies() throws Exception {
+		DataExporter dex = new DataExporter(eappfisaDataSource());
+		final String tableName = "TCRB_ORDER";
+		final String fileName = "/Users/edison/Tmp/export/tcrb_order.xml";
+		dex.exportTableAndDependencies(tableName, fileName);
+	}
+
+	private DataSource eappfisaDataSource() {
 		final String ORA_DRIVER_CLASS = "oracle.jdbc.driver.OracleDriver";
 		final String BASE_URL = "jdbc:oracle:thin:@";
 
@@ -32,4 +49,25 @@ public class DataExporterTest {
 
 		return ds;
 	}
+
+	private DataSource eefisaDataSource() {
+		final String ORA_DRIVER_CLASS = "oracle.jdbc.driver.OracleDriver";
+		final String BASE_URL = "jdbc:oracle:thin:@";
+
+		final String ip = "192.168.0.164";
+		final String serviceName = "modinter";
+		final String port = "1521";
+		final String userName = "EEFISA_V38";
+		final String password = "EEFISA_V38";
+
+		String url = BASE_URL.concat(ip).concat(":").concat(port).concat(":").concat(serviceName);
+		BasicDataSource ds = new BasicDataSource();
+		ds.setDriverClassName(ORA_DRIVER_CLASS);
+		ds.setUrl(url);
+		ds.setUsername(userName);
+		ds.setPassword(password);
+
+		return ds;
+	}
+
 }
