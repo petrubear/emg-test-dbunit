8c054a34664ac1f8860715de1edef079daa6bfc2
petrubear <edison.martinez@gmail.com>
2016-05-24 11:53:05 -0500

    add option for table qualifiednames

--- src/main/java/emg/demo/dbunit/DataExporter.java
+++ src/main/java/emg/demo/dbunit/DataExporter.java
@@ -1,10 +1,5 @@
 package emg.demo.dbunit;
 
-import java.io.FileOutputStream;
-import java.sql.Connection;
-
-import javax.sql.DataSource;
-
 import org.dbunit.database.DatabaseConfig;
 import org.dbunit.database.DatabaseConnection;
 import org.dbunit.database.IDatabaseConnection;
@@ -14,60 +9,93 @@ import org.dbunit.dataset.IDataSet;
 import org.dbunit.dataset.xml.FlatXmlDataSet;
 import org.dbunit.ext.oracle.OracleDataTypeFactory;
 
+import javax.sql.DataSource;
+import java.io.FileOutputStream;
+import java.sql.Connection;
+import java.util.Set;
+
 public class DataExporter {
-	private DataSource datasource;
+    private DataSource datasource;
 
-	public DataSource getDatasource() {
-		return datasource;
-	}
+    public DataSource getDatasource() {
+        return datasource;
+    }
 
-	public void setDatasource(DataSource datasource) {
-		this.datasource = datasource;
-	}
+    public void setDatasource(DataSource datasource) {
+        this.datasource = datasource;
+    }
 
-	public DataExporter(DataSource datasource) {
-		this.datasource = datasource;
-	}
+    public DataExporter(DataSource datasource) {
+        this.datasource = datasource;
+    }
 
-	public void exportTableAndDependencies(String tableName, String fileName) throws Exception {
-		try (Connection jdbcConnection = datasource.getConnection()) {
-			IDatabaseConnection connection = new DatabaseConnection(jdbcConnection);
-			DatabaseConfig memConfig = connection.getConfig();
-			memConfig.setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new OracleDataTypeFactory());
+    public void exportTableAndDependencies(String tableName, String fileName, boolean useQualifiedTableNames) throws Exception {
+        try (Connection jdbcConnection = datasource.getConnection()) {
+            IDatabaseConnection connection = new DatabaseConnection(jdbcConnection);
+            DatabaseConfig memConfig = connection.getConfig();
+            memConfig.setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new OracleDataTypeFactory());
+            memConfig.setProperty(DatabaseConfig.FEATURE_QUALIFIED_TABLE_NAMES, useQualifiedTableNames);
+            String[] depTableNames = TablesDependencyHelper.getAllDependentTables(connection, tableName);
+            IDataSet depDataset = connection.createDataSet(depTableNames);
+            FlatXmlDataSet.write(depDataset, new FileOutputStream(fileName));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 
-			String[] depTableNames = TablesDependencyHelper.getAllDependentTables(connection, tableName);
-			IDataSet depDataset = connection.createDataSet(depTableNames);
-			FlatXmlDataSet.write(depDataset, new FileOutputStream(fileName));
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-	}
+    public void exportSingleTable(String tableName, String fileName, boolean useQualifiedTableNames) throws Exception {
+        try (Connection jdbcConnection = datasource.getConnection()) {
+            IDatabaseConnection connection = new DatabaseConnection(jdbcConnection);
+            DatabaseConfig memConfig = connection.getConfig();
+            memConfig.setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new OracleDataTypeFactory());
+            memConfig.setProperty(DatabaseConfig.FEATURE_QUALIFIED_TABLE_NAMES, useQualifiedTableNames);
+            String[] depTableNames = new String[]{tableName};
+            IDataSet depDataset = connection.createDataSet(depTableNames);
+            FlatXmlDataSet.write(depDataset, new FileOutputStream(fileName));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 
-	public void exportFullDatabase(String fileName) throws Exception {
-		try (Connection jdbcConnection = datasource.getConnection()) {
-			IDatabaseConnection connection = new DatabaseConnection(jdbcConnection);
-			DatabaseConfig memConfig = connection.getConfig();
-			memConfig.setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new OracleDataTypeFactory());
+    public void exportFullDatabase(String fileName) throws Exception {
+        try (Connection jdbcConnection = datasource.getConnection()) {
+            IDatabaseConnection connection = new DatabaseConnection(jdbcConnection);
+            DatabaseConfig memConfig = connection.getConfig();
+            memConfig.setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new OracleDataTypeFactory());
 
-			IDataSet fullDataSet = connection.createDataSet();
-			FlatXmlDataSet.write(fullDataSet, new FileOutputStream(fileName));
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-	}
+            IDataSet fullDataSet = connection.createDataSet();
+            FlatXmlDataSet.write(fullDataSet, new FileOutputStream(fileName));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 
-	public void exportTableQuery(String table, String sql, String fileName) throws Exception {
-		try (Connection jdbcConnection = datasource.getConnection()) {
-			IDatabaseConnection connection = new DatabaseConnection(jdbcConnection);
-			DatabaseConfig memConfig = connection.getConfig();
-			memConfig.setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new OracleDataTypeFactory());
-			
-			QueryDataSet partialDataSet = new QueryDataSet(connection);
-			partialDataSet.addTable(table, sql);
-			// partialDataSet.addTable("BAR");
-			FlatXmlDataSet.write(partialDataSet, new FileOutputStream(fileName));
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-	}
+    public void exportTableQuery(String table, String sql, String fileName) throws Exception {
+        try (Connection jdbcConnection = datasource.getConnection()) {
+            IDatabaseConnection connection = new DatabaseConnection(jdbcConnection);
+            DatabaseConfig memConfig = connection.getConfig();
+            memConfig.setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new OracleDataTypeFactory());
+
+            QueryDataSet partialDataSet = new QueryDataSet(connection);
+            partialDataSet.addTable(table, sql);
+            // partialDataSet.addTable("BAR");
+            FlatXmlDataSet.write(partialDataSet, new FileOutputStream(fileName));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void exportTableAndDependencies(String tableName, String fileName, Set allowedIDs, boolean useQualifiedTableNames) throws Exception {
+        try (Connection jdbcConnection = datasource.getConnection()) {
+            IDatabaseConnection connection = new DatabaseConnection(jdbcConnection);
+            DatabaseConfig memConfig = connection.getConfig();
+            memConfig.setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new OracleDataTypeFactory());
+            memConfig.setProperty(DatabaseConfig.FEATURE_QUALIFIED_TABLE_NAMES, useQualifiedTableNames);
+            //String[] depTableNames = TablesDependencyHelper.getAllDependentTables(connection, tableName);
+            IDataSet depDataset = TablesDependencyHelper.getAllDataset(connection, tableName, allowedIDs);
+            FlatXmlDataSet.write(depDataset, new FileOutputStream(fileName));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 }
--- src/test/java/emg/demo/dbunit/DataExporterTest.java
+++ src/test/java/emg/demo/dbunit/DataExporterTest.java
@@ -1,73 +1,94 @@
 package emg.demo.dbunit;
 
-import javax.sql.DataSource;
-
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.junit.Test;
 
-public class DataExporterTest {
-	@Test
-	public void testExportTableQuery() throws Exception {
-		DataExporter dex = new DataExporter(eefisaDataSource());
-		final String table = "UniversalCatalog";
-		final String sql = "SELECT UCT.FIELD_NAME AS FIELD_NAME, UCT.TARGET_TABLE AS T_TABLE, "
-				+ "UCT.TARGET_FIELD AS T_FIELD, SSY.DATA_SOURCE_ID AS DATA_SOURCE, "
-				+ "UCT.APPLICATION_ID AS APPLICATION_ID, " + "ELE.NAME AS ELEMENT_NAME, " + "FMT.NAME AS FORMAT_NAME, "
-				+ "REG.NAME REGISTER_NAME " + "FROM " + "TPAR_UNIVERSAL_CATALOG UCT, " + "TAPD_APPLICATION APP, "
-				+ "TAPD_SOURCE_SYSTEM SSY, " + "TPAR_ELEMENT ELE, " + "TPAR_REGISTER REG, " + "TPAR_FILE_FORMAT FMT "
-				+ "WHERE " + "UCT.APPLICATION_ID = APP.APPLICATION_ID "
-				+ "AND APP.SOURCE_SYSTEM_ID = SSY.SOURCE_SYSTEM_ID " + "AND UCT.FIELD_NAME = ELE.FIELD_NAME "
-				+ "AND REG.ID_REGISTER = ELE.ID_REGISTER " + "AND FMT.ID_FILE_FORMAT = REG.ID_FILE_FORMAT";
-		final String fileName = "/Users/edison/Tmp/export/ucatalog.xml";
-		dex.exportTableQuery(table, sql, fileName);
-	}
+import javax.sql.DataSource;
+import java.util.HashSet;
+import java.util.Set;
 
-	@Test
-	public void testExportTableAndDependencies() throws Exception {
-		DataExporter dex = new DataExporter(eappfisaDataSource());
+public class DataExporterTest {
+    @Test
+    public void testExportTableQuery() throws Exception {
+        DataExporter dex = new DataExporter(eappfisaDataSource());
+        final String table = "UniversalCatalog";
+        final String sql = "SELECT UCT.FIELD_NAME AS FIELD_NAME, UCT.TARGET_TABLE AS T_TABLE, "
+                + "UCT.TARGET_FIELD AS T_FIELD, SSY.DATA_SOURCE_ID AS DATA_SOURCE, "
+                + "UCT.APPLICATION_ID AS APPLICATION_ID, " + "ELE.NAME AS ELEMENT_NAME, " + "FMT.NAME AS FORMAT_NAME, "
+                + "REG.NAME REGISTER_NAME " + "FROM " + "TPAR_UNIVERSAL_CATALOG UCT, " + "TAPD_APPLICATION APP, "
+                + "TAPD_SOURCE_SYSTEM SSY, " + "TPAR_ELEMENT ELE, " + "TPAR_REGISTER REG, " + "TPAR_FILE_FORMAT FMT "
+                + "WHERE " + "UCT.APPLICATION_ID = APP.APPLICATION_ID "
+                + "AND APP.SOURCE_SYSTEM_ID = SSY.SOURCE_SYSTEM_ID " + "AND UCT.FIELD_NAME = ELE.FIELD_NAME "
+                + "AND REG.ID_REGISTER = ELE.ID_REGISTER " + "AND FMT.ID_FILE_FORMAT = REG.ID_FILE_FORMAT";
+
+        final String fileName = "/Users/edison/Tmp/export/ucatalog.xml";
+        dex.exportTableQuery(table, sql, fileName);
+    }
+
+    @Test
+    public void testExportSingleTAble() throws Exception {
+        DataExporter dex = new DataExporter(eappfisaDataSource());
+        final String tableName = "EAPPFISA_V38.TCRB_TEMPLATE_SERVICE";
+        final String fileName = "/Users/edison/Tmp/export/TCRB_TEMPLATE_SERVICE.xml";
+        dex.exportSingleTable(tableName, fileName, true);
+    }
+
+    @Test
+    public void testExportTableAndDependencies() throws Exception {
+        DataExporter dex = new DataExporter(eappfisaDataSource());
 		final String tableName = "TCRB_ORDER";
 		final String fileName = "/Users/edison/Tmp/export/tcrb_order.xml";
-		dex.exportTableAndDependencies(tableName, fileName);
-	}
+        dex.exportTableAndDependencies(tableName, fileName, false);
+    }
 
-	private DataSource eappfisaDataSource() {
-		final String ORA_DRIVER_CLASS = "oracle.jdbc.driver.OracleDriver";
-		final String BASE_URL = "jdbc:oracle:thin:@";
+    @Test
+    public void testExportTableAndDependenciesSet() throws Exception {
+        DataExporter dex = new DataExporter(eappfisaDataSource());
+        Set<String> set = new HashSet<>();
+        final String tableName = "TCRB_TEMPLATE_SERVICE";
+        final String fileName = "/Users/edison/Tmp/export/TCRB_TEMPLATE_SERVICE.xml";
+        set.add("1");
+        dex.exportTableAndDependencies(tableName, fileName, set, false);
+    }
 
-		final String ip = "192.168.0.164";
-		final String serviceName = "modinter";
-		final String port = "1521";
-		final String userName = "EAPPFISA_V38";
-		final String password = "EAPPFISA_V38";
+    private DataSource eappfisaDataSource() {
+        final String ORA_DRIVER_CLASS = "oracle.jdbc.driver.OracleDriver";
+        final String BASE_URL = "jdbc:oracle:thin:@";
 
-		String url = BASE_URL.concat(ip).concat(":").concat(port).concat(":").concat(serviceName);
-		BasicDataSource ds = new BasicDataSource();
-		ds.setDriverClassName(ORA_DRIVER_CLASS);
-		ds.setUrl(url);
-		ds.setUsername(userName);
-		ds.setPassword(password);
+        final String ip = "192.168.0.164";
+        final String serviceName = "modinter";
+        final String port = "1521";
+        final String userName = "EAPPFISA_V38";
+        final String password = "eappfisauni38";
 
-		return ds;
-	}
+        String url = BASE_URL.concat(ip).concat(":").concat(port).concat(":").concat(serviceName);
+        BasicDataSource ds = new BasicDataSource();
+        ds.setDriverClassName(ORA_DRIVER_CLASS);
+        ds.setUrl(url);
+        ds.setUsername(userName);
+        ds.setPassword(password);
 
-	private DataSource eefisaDataSource() {
-		final String ORA_DRIVER_CLASS = "oracle.jdbc.driver.OracleDriver";
-		final String BASE_URL = "jdbc:oracle:thin:@";
+        return ds;
+    }
 
-		final String ip = "192.168.0.164";
-		final String serviceName = "modinter";
-		final String port = "1521";
-		final String userName = "EEFISA_V38";
-		final String password = "EEFISA_V38";
+    private DataSource eefisaDataSource() {
+        final String ORA_DRIVER_CLASS = "oracle.jdbc.driver.OracleDriver";
+        final String BASE_URL = "jdbc:oracle:thin:@";
 
-		String url = BASE_URL.concat(ip).concat(":").concat(port).concat(":").concat(serviceName);
-		BasicDataSource ds = new BasicDataSource();
-		ds.setDriverClassName(ORA_DRIVER_CLASS);
-		ds.setUrl(url);
-		ds.setUsername(userName);
-		ds.setPassword(password);
+        final String ip = "192.168.0.164";
+        final String serviceName = "modinter";
+        final String port = "1521";
+        final String userName = "EEFISA_V38";
+        final String password = "eefisauni38";
 
-		return ds;
-	}
+        String url = BASE_URL.concat(ip).concat(":").concat(port).concat(":").concat(serviceName);
+        BasicDataSource ds = new BasicDataSource();
+        ds.setDriverClassName(ORA_DRIVER_CLASS);
+        ds.setUrl(url);
+        ds.setUsername(userName);
+        ds.setPassword(password);
+
+        return ds;
+    }
 
 }
